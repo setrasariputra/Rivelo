600f95a76d25f813edc91abe361b9719fe0deb46
Wisnu K. Ardianto <setrasariputra@gmail.com>
2025-01-13 20:48:37 +0700

    Create variable personalities dan quizOptions

--- iPersonic/script.js
+++ iPersonic/script.js
@@ -1,22 +1,41 @@
 class PersonalityQuiz { // class or module
     constructor() {
         this.personalities = [
-            {'url': '1.html', 'label': 'Idealis Penyelaras'},
-            {'url': '2.html', 'label': 'Idealis Terlibat'},
-            {'url': '3.html', 'label': 'Pemikir Dinamis'},
-            {'url': '4.html', 'label': 'Pemikir Pendobrak'},
-            {'url': '5.html', 'label': 'Idealis Spontan'},
-            {'url': '6.html', 'label': 'Pelaku Santai'},
-            {'url': '7.html', 'label': 'Realis Sosial'},
-            {'url': '8.html', 'label': 'Realis Bertekad'},
-            {'url': '9.html', 'label': 'Pelaku Bersemangat'},
-            {'url': '10.html', 'label': 'Pelaku Individualistis'},
-            {'url': '11.html', 'label': 'Pelaku Peka'},
-            {'url': '12.html', 'label': 'Realis Baik Hati'},
-            {'url': '13.html', 'label': 'Realis Tepercaya'},
-            {'url': '14.html', 'label': 'Pemikir Mandiri'},
-            {'url': '15.html', 'label': 'Pemikir Analitis'},
-            {'url': '16.html', 'label': 'Idealis Pemimpi'}
+            {'fx': 'BBBB', 'url': '1.html', 'label': 'Idealis Penyelaras'},
+            {'fx': 'ABBB', 'url': '2.html', 'label': 'Idealis Terlibat'},
+            {'fx': 'ABAB', 'url': '3.html', 'label': 'Pemikir Dinamis'},
+            {'fx': 'ABAA', 'url': '4.html', 'label': 'Pemikir Pendobrak'},
+            {'fx': 'ABBA', 'url': '5.html', 'label': 'Idealis Spontan'},
+            {'fx': 'AABA', 'url': '6.html', 'label': 'Pelaku Santai'},
+            {'fx': 'AABB', 'url': '7.html', 'label': 'Realis Sosial'},
+            {'fx': 'AAAB', 'url': '8.html', 'label': 'Realis Bertekad'},
+            {'fx': 'AAAA', 'url': '9.html', 'label': 'Pelaku Bersemangat'},
+            {'fx': 'BAAA', 'url': '10.html', 'label': 'Pelaku Individualistis'},
+            {'fx': 'BABA', 'url': '11.html', 'label': 'Pelaku Peka'},
+            {'fx': 'BABB', 'url': '12.html', 'label': 'Realis Baik Hati'},
+            {'fx': 'BAAB', 'url': '13.html', 'label': 'Realis Tepercaya'},
+            {'fx': 'BBAB', 'url': '14.html', 'label': 'Pemikir Mandiri'},
+            {'fx': 'BBAA', 'url': '15.html', 'label': 'Pemikir Analitis'},
+            {'fx': 'BBBA', 'url': '16.html', 'label': 'Idealis Pemimpi'}
+        ];
+
+        this.quizOptions = [
+            {
+                1: "Saya agak ramah, membuka hati dan saya suka berada bersama-sama dengan orang lain.",
+                2: "Saya membutuhkan banyak waktu untuk sendirian dan agak hati-hati untuk memulai hubungan baru.",
+            },
+            {
+                1: "Saya lebih suka bekerja praktis, menghasilkan hasil yang nyata.",
+                2: "Saya lebih suka bekerja secara teori, mengembangkan ide-ide dan konsep baru.",
+            },
+            {
+                1: "Saya pintar dalam hal analisis dan logika dan ketika saya ragu, saya membiarkan diri saya dipandu oleh otak saya.",
+                2: "Saya sangat sensitif dan emosional dan bila ragu, aku membiarkan diriku dibimbing oleh hati saya."
+            },
+            {
+                1: "Saya orang yang fleksibel dan spontan, kadang-kadang agak kacau.",
+                2: "Saya orang yang handal dan terorganisir dengan baik. Saya lebih memilih untuk merencanakan masa depan."
+            }
         ];
         
         this.init();
@@ -33,7 +52,7 @@ class PersonalityQuiz { // class or module
         
         // start loop variable this.personalities
         for(let i = 0; i < this.personalities.length; i++) {
-            dropdownMenuHTML += "<li><a href='#'>"+this.personalities[i]['label']+"</a></li>";
+            dropdownMenuHTML += "<li><a href='"+this.personalities[i]['url']+"'>"+this.personalities[i]['label']+"</a></li>";
         }
 
         dropdownMenuHTML += "</ul>";
